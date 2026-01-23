0e75985a74c7863a30654f1c9808289c90e06a72
shivanimahar <shivanimahar.offc@gmail.com>
2026-01-23 23:02:07 +0530

    ch1

---
+++ Chapter 1/04_sizeof.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    printf("Size of int: %zu byte\n", sizeof(int));
+    printf("Size of char: %zu byte\n", sizeof(char));
+    printf("Size of float: %zu byte\n", sizeof(float));
+
+}
---
+++ Chapter 1/05_comments.c
@@ -0,0 +1,13 @@
+
+/*
+this
+is
+mutiline comm
+*/
+#include<stdio.h>
+
+int main(){
+    // I am writing this just for fun. This  line doesnt do anything
+    printf("Hello World"); 
+    return 0;
+}
