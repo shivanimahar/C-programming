b69ae8980c64d1c1604c181a14808df3aa9681eb
shivanimahar <shivanimahar.offc@gmail.com>
2026-02-12 21:17:58 +0530

    inital commit

--- Chapter 11/05_realloc.c
+++ Chapter 11/05_realloc.c
@@ -7,12 +7,15 @@ int main()
     ptr = fopen("harry.txt", "r");
     while (1)
     {
+
         ch = fgetc(ptr); 
         printf("%c", ch);
         // when all the content of a file has been read break
         if (ch == EOF)
         {
+            
             break;
+
         }
     }
         return 0;
