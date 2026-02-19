4edcfa045751ed458087648a6970fc0d5d0415d1
shivanimahar <shivanimahar.offc@gmail.com>
2026-02-19 10:45:22 +0530

    chapter 7 completed

--- Chapter 7 - PS/05_problem5.c
+++ Chapter 7 - PS/05_problem5.c
@@ -1,7 +1,33 @@
 // // 5. Write a program containing a function which reverses the array passed to it.
 // #include<stdio.h>
 
-// int main(){
-//     int a[] = 
-// }
+#include <stdio.h>
 
+void printArray(int a[], int n){ // int n is size of an array
+    for (int i = 0; i < n; i++)
+    {
+       printf("%d ", a[i]); 
+    }
+    printf("\n");
+}
+
+void reverse(int arr[], int n){
+   /* for  i from 0 to n/2
+    arr[i] arr[n-i-1] 
+    */
+   int temp; //temporary variable 1, 2, 3, 4, 5, 6, 7
+   for (int i = 0; i < n/2; i++) 
+   { 
+    temp = arr[i]; 
+    arr[i] = arr[n-i-1];
+    arr[n-i-1] = temp; 
+   }
+   
+}
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6 };
+    printArray(arr, 6);
+    reverse(arr, 6);
+    printArray(arr, 6);
+    return 0;
+}
--- Chapter 7 - PS/08_problem8.c
+++ Chapter 7 - PS/08_problem8.c
@@ -1,1 +1,27 @@
 // 8. Repeat problem 7 for a custom input given by the user.
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][10];
+    int mul[] = {2, 7, 9};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            arr[i][j] = mul[i] * (j + 1);
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("The value of arr[i][j] is %d\n", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
