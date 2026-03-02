62288c34e83589f857f2f6f2b2df95a20354725a
shivanimahar <shivanimahar.offc@gmail.com>
2026-03-02 23:00:55 +0530

    ques

--- Practice Ques 3(functions)/21_problem21.c
+++ Practice Ques 3(functions)/21_problem21.c
@@ -4,3 +4,19 @@
 
     Test: sumDigits(1234) = 10
 */
+
+#include <stdio.h>
+
+int sumDigits(int n) {
+    if (n == 0)          // Base case
+        return 0;
+    else 
+        return (n % 10) + sumDigits(n / 10);  // 4 + 123
+        // Recursive call
+}
+
+int main() {
+    int num = 1234;
+    printf("Sum of digits: %d", sumDigits(num));
+    return 0;
+}
