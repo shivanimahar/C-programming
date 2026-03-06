ad718d6975de89936f9a81b8205fa783349cedb3
shivanimahar <shivanimahar.offc@gmail.com>
2026-03-06 23:28:03 +0530

    ques

--- Practice Ques 3(functions)/25_problem25.c
+++ Practice Ques 3(functions)/25_problem25.c
@@ -1,5 +1,8 @@
 /*
+
 24. Write a C program to show variable shadowing: declare a global int x = 100
     and a local int x = 200 inside a function. Print both from the function
     (local) and from main() (global).
+
 */
+#include<stdio.h>
